bf5bb01df902cf9b906c3453df923db7fc22861b
Valeriy Kozyar <51660599+KozyarValeriy@users.noreply.github.com>
2020-02-13 13:45:46 +0300

    Update dim_suppliers.sql

--- task_1/dim_suppliers.sql
+++ task_1/dim_suppliers.sql
@@ -29,7 +29,7 @@ declare
 	name 				VARCHAR2(40); 
 	country				VARCHAR2(40); 
 	city				varchar2(40);
-    cities     			arr_type_city;
+        cities     			arr_type_city;
 	city_index			number;
 	flag				number(1);
  
@@ -41,29 +41,26 @@ begin
     cities(5) := 'Астрахань';
     cities(6) := 'Тула';
     
-	country := 'Россия';
+    country := 'Россия';
 	
 	-- получение всех доступных кодов категорий
-	for rowCategory in (select distinct category_code from dim_products) loop
-		-- получаем 1, если этот код уже есть в поставщиках, иначе 0
-		select case when exists(select * from DIM_SUPPLIERS 
-							  where category = rowCategory.category_code)
-				   then 1 
-				   else 0
-			   end  
-			into flag
-			from dual;
-			
-		-- если кода нет, добавляем в таблицу
-		if flag = 0 then
-			city_index := mod(round(DBMS_RANDOM.VALUE * 100), cities.count) + 1;
-			city := cities(city_index);      
-
-			name := 'OOO '||DBMS_RANDOM.STRING('a', 4);   
-		
-			insert into DIM_SUPPLIERS values (suppliers_s.NEXTVAL, rowCategory.category_code, 
-											  name, country, city, systimestamp);
-		end if;
-	end loop;
-	commit;
+    for rowCategory in (select distinct category_code from dim_products) loop
+        -- получаем 1, если этот код уже есть в поставщиках, иначе 0
+	select case when exists(select * from DIM_SUPPLIERS 
+		      where category = rowCategory.category_code)
+	          then 1 
+	          else 0
+	       end  
+	into flag
+	from dual;
+	-- если кода нет, добавляем в таблицу
+	if flag = 0 then
+	    city_index := mod(round(DBMS_RANDOM.VALUE * 100), cities.count) + 1;
+	    city := cities(city_index);      
+	    name := 'OOO '||DBMS_RANDOM.STRING('a', 4);   
+	    insert into DIM_SUPPLIERS values (suppliers_s.NEXTVAL, rowCategory.category_code, 
+	   				      name, country, city, systimestamp);
+	end if;
+    end loop;
+    commit;
 end;
