20ccd09005636804ba4565c7bafb21bb39c39fe4
Vladimir Shagdurov <44134057+Vianvong@users.noreply.github.com>
2020-02-18 18:01:42 +0300

    Продюсер для всех таблиц

--- task_2/producer_ok.py
+++ task_2/producer_ok.py
@@ -2,42 +2,58 @@ from pyspark.shell import spark
 import pyspark.sql.functions as sf
 from kafka import KafkaProducer
 import json
-
+import decimal
+import datetime
 
 def serializer():
     return str.encode
 
 
-# Serialization in JSON
-def build_JSON(event_time, event_id, product_id, category_id, category_code, brand, price, customer_id,
-               customer_session):
-    data = dict(event_time=event_time, event_id=event_id, product_id=product_id, category_id=category_id,
-                category_code=category_code, brand=brand, price=price, customer_id=customer_id,
-                customer_session=customer_session)
-    return json.dumps(data)
+def build_JSON(x):
+    return json.dumps(x)
+
+#
+ARR_TOPIC = ['dim_customers', 'dim_products', 'dim_suppliers']
+#
+DATABASE_SOURCE = {'url': 'jdbc:oracle:thin:@192.168.88.252:1521:oradb', 'user': 'test_user', 'password': 'test_user'}
+#
+DATABASE_DESTINATION = {'url': 'jdbc:oracle:thin:@192.168.88.95:1521:orcl', 'user': 'test_user',
+                        'password': 'test_user'}
+#
+DICT_INCREMENTAL_FIELD = {'fct_events': 'event_id', 'dim_customers': 'last_update_date',
+                          'dim_products': 'last_update_date', 'dim_suppliers': 'last_update_date'}
+#
+DICT_FIELDS = {
+    'fct_events': {'event_time': None, 'event_type': None, 'event_id': None, 'product_id': None, 'category_id': None,
+                   'category_code': None, 'brand': None, 'price': None, 'customer_id': None},
+    'dim_customers': {'customer_id': None, 'country': None, 'city': None, 'phone': None, 'first_name': None,
+                      'last_name': None, 'mail': None, 'last_update_date': None},
+    'dim_products': {'product_id': None, 'category_id': None, 'category_code': None, 'brand': None, 'description': None,
+                     'name': None, 'price': None, 'last_update_date': None},
+    'dim_suppliers': {'suppliers_id': None, 'category': None, 'name': None, 'country': None, 'city': None,
+                      'last_update_date': None}
+}
 
 
 # The implementation of the producer
-def producer_to_Kafka(dfResult):
+def sending_to_Kafka(dfResult, topic):
     producer = KafkaProducer(bootstrap_servers=['cdh631.itfbgroup.local:9092'],
-                             key_serializer=serializer(), value_serializer=serializer())
+                             value_serializer=serializer())
 
     for i in range(len(dfResult)):
         try:
-            event_time = dfResult[i]['event_time']
-            event_id = int(dfResult[i]['event_id'])
-            product_id = int(dfResult[i]['product_id'])
-            category_id = int(dfResult[i]['category_id'])
-            category_code = dfResult[i]['category_code']
-            brand = dfResult[i]['brand']
-            price = int(dfResult[i]['price'])
-            customer_id = int(dfResult[i]['customer_id'])
-            customer_session = dfResult[i]['customer_session']
+            for field in DICT_FIELDS[topic].keys():
+                tmp = dfResult[i][field.upper()]
+                if isinstance(tmp, decimal.Decimal):
+                    DICT_FIELDS[topic][field] = int(tmp)
+                elif isinstance(tmp, datetime.datetime):
+                    DICT_FIELDS[topic][field] = str(tmp)
+                else:
+                    DICT_FIELDS[topic][field] = tmp
 
-            values = build_JSON(event_time, event_id, product_id, category_id, category_code, brand, price, customer_id,
-                                customer_session)
+            values = build_JSON(DICT_FIELDS[topic])
             print(values)
-            future = producer.send(TOPIC, key=str('fact_table'), value=values)
+            future = producer.send(topic, key=topic, value=values)
 
         except Exception as e:
             print('--> It seems an Error occurred: {}'.format(e))
@@ -45,34 +61,38 @@ def producer_to_Kafka(dfResult):
     producer.flush()
 
 
-if __name__ == "__main__":
-    # Topic for sending data
-    TOPIC = 'vshagFirstTopic'
+if __name__ == '__main__':
+    for topic in ARR_TOPIC:
+        # Creating a dataframe for the source table
+        df0 = spark.read \
+            .format('jdbc') \
+            .option('driver', 'oracle.jdbc.OracleDriver') \
+            .option('url', DATABASE_SOURCE['url']) \
+            .option('dbtable', topic) \
+            .option('user', DATABASE_SOURCE['user']) \
+            .option('password', DATABASE_SOURCE['password']) \
+            .load()
 
-    # Creating a dataframe for the source table
-    df0 = spark.read \
-        .format("jdbc") \
-        .option("url", "jdbc:oracle:thin:@192.168.88.252:1521/oradb") \
-        .option("dbtable", "test_events") \
-        .option("user", "test_user") \
-        .option("password", "test_user") \
-        .option("driver", "oracle.jdbc.driver.OracleDriver") \
-        .load()
+        # Creating a dataframe for the destination table
+        df1 = spark.read \
+            .format('jdbc') \
+            .option('driver', 'oracle.jdbc.OracleDriver') \
+            .option('url', DATABASE_DESTINATION['url']) \
+            .option('dbtable', topic.upper()) \
+            .option('user', DATABASE_DESTINATION['user']) \
+            .option('password', DATABASE_DESTINATION['password']) \
+            .load()
 
-    # Creating a dataframe for the recipient table
-    df1 = spark.read \
-        .format("jdbc") \
-        .option("url", "jdbc:oracle:thin:@192.168.88.98:1521:orcl") \
-        .option("dbtable", "test_events_target") \
-        .option("user", "test_user") \
-        .option("password", "test_user") \
-        .option("driver", "oracle.jdbc.driver.OracleDriver") \
-        .load()
+        incr_field = DICT_INCREMENTAL_FIELD[topic]
 
-    # Finding the last event in the recipient table
-    row1 = df1.agg({"event_id": "max"}).collect()[0]
-    maxID = int(row1["max(event_id)"])
+        max_value = df1.agg({incr_field: 'max'}).collect()[0][0]
+        print(max_value)
+        print(type(max_value))
+        if max_value is None:
+            if isinstance(max_value, int):
+                max_value = 0
+            else:
+                pass
+        dfResult = df0.where(sf.col(incr_field) > max_value).collect()
+        sending_to_Kafka(dfResult, topic)
 
-    # Creating a dataframe for transmission
-    dfResult = df0.where(sf.col("event_id") > maxID).collect()
-    producer_to_Kafka(dfResult)
