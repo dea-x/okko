bf3d326eb8ed8b25451b186a910f95787838a611
Valeriy Kozyar <51660599+KozyarValeriy@users.noreply.github.com>
2020-02-18 16:14:14 +0300

    Add files via upload

--- task_4/PARTITION_TABLE.sql
+++ task_4/PARTITION_TABLE.sql
@@ -17,42 +17,44 @@ CREATE TABLE FCT_EVENTS_part(
     brand              VARCHAR2(30),
     price              NUMBER,
     customer_id        NUMBER,
-    mounth GENERATED ALWAYS AS (TO_CHAR(event_time, 'mm')) VIRTUAL, -- виртуальный столбец
+    -- mounth GENERATED ALWAYS AS (TO_CHAR(event_time, 'mm')) VIRTUAL, -- виртуальный столбец
     CONSTRAINT FCT_EVENTS_part_pk PRIMARY KEY (event_id)
 )
-PARTITION BY LIST(mounth) (
-    PARTITION JAN VALUES ('01'),
-    PARTITION FEB VALUES ('02'),
-    PARTITION MAR VALUES ('03'),
-    PARTITION APR VALUES ('04'),
-    PARTITION MAY VALUES ('05'),
-    PARTITION JUN VALUES ('06'),
-    PARTITION JUL VALUES ('07'),
-    PARTITION AUG VALUES ('08'),
-    PARTITION SEP VALUES ('09'),
-    PARTITION OCT VALUES ('10'),
-    PARTITION NOV VALUES ('11'),
-    PARTITION DEC VALUES ('12')
+PARTITION BY RANGE(event_time)(
+    PARTITION JAN values less than (to_date('01.02.2020','DD.MM.YYYY')),
+    PARTITION FEB values less than (to_date('01.03.2020','DD.MM.YYYY')),
+    PARTITION MAR values less than (to_date('01.04.2020','DD.MM.YYYY')),
+    PARTITION APR values less than (to_date('01.05.2020','DD.MM.YYYY')),
+    PARTITION MAY values less than (to_date('01.06.2020','DD.MM.YYYY')),
+    PARTITION JUL values less than (to_date('01.07.2020','DD.MM.YYYY')),
+    PARTITION AUG values less than (to_date('01.08.2020','DD.MM.YYYY')),
+    PARTITION SEP values less than (to_date('01.09.2020','DD.MM.YYYY')),
+    PARTITION OCT values less than (to_date('01.10.2020','DD.MM.YYYY')),
+    PARTITION JUN values less than (to_date('01.11.2020','DD.MM.YYYY')),
+    PARTITION NOV values less than (to_date('01.12.2020','DD.MM.YYYY')),
+    PARTITION DEC values less than (maxvalue)
 );
 
 -- Для исследования влияния времени лучше использовать разбиение по хеш функции.
 -- У нас не будет данных за весь год, а при разбиении по месяцам это приведет конкретной
 -- неравномерному разбиению.
 -- Разбиение по хешу будет более равномерно. Так будет лучше видно влияние на время обработки запроса.
--- CREATE TABLE FCT_EVENTS_part(
---     event_time DATE,
---     event_type VARCHAR2(20),
---     event_id NUMBER,
---     product_id NUMBER,
---     category_id NUMBER,
---     category_code VARCHAR2(25),
---     brand VARCHAR2(30),
---     price NUMBER,
---     customer_id NUMBER,
---     mounth GENERATED ALWAYS AS (TO_CHAR(event_time, 'mm')) VIRTUAL, -- виртуальный столбец
---     CONSTRAINT FCT_EVENTS_part_pk PRIMARY KEY (event_id)
--- )
--- PARTITION BY HASH(event_time) PARTITIONS 12;
+/*
+CREATE TABLE FCT_EVENTS_part(
+    event_time DATE,
+    event_type VARCHAR2(20),
+    event_id NUMBER,
+    product_id NUMBER,
+    category_id NUMBER,
+    category_code VARCHAR2(25),
+    brand VARCHAR2(30),
+    price NUMBER,
+    customer_id NUMBER,
+    mounth GENERATED ALWAYS AS (TO_CHAR(event_time, 'mm')) VIRTUAL, -- виртуальный столбец
+    CONSTRAINT FCT_EVENTS_part_pk PRIMARY KEY (event_id)
+)
+PARTITION BY HASH(event_time) PARTITIONS 12;
+*/
 
 -- Вставка данных из обычной таблицы
 INSERT INTO FCT_EVENTS_part (event_time, event_type, event_id, product_id, category_id, category_code, brand, price, customer_id)
