248a498f618a9ef769e6e16343b134ed61a9d60c
Pavel Gubkin <57576761+dea-x@users.noreply.github.com>
2020-02-18 11:30:31 +0300

    Update okko_job.sql

--- task_1/okko_job.sql
+++ task_1/okko_job.sql
@@ -1,11 +1,11 @@
 BEGIN
-DBMS_SCHEDULER.CREATE_JOB(
-JOB_NAME => 'okko_job',
-JOB_TYPE => 'PLSQL_BLOCK',
-JOB_ACTION => 'BEGIN test_user.okko.FILL_customers(100); test_user.okko.FILL_products(100); test_user.okko.FILL_fct_events; END;',
-START_DATE => SYSTIMESTAMP,
-REPEAT_INTERVAL => 'FREQ=MINUTELY; INTERVAL=5',
-END_DATE => SYSTIMESTAMP + INTERVAL '30' day,
-COMMENTS => 'Insert new data into source DB',
-ENABLED => TRUE);
+  DBMS_SCHEDULER.CREATE_JOB(
+  JOB_NAME => 'okko_job',
+  JOB_TYPE => 'PLSQL_BLOCK',
+  JOB_ACTION => 'BEGIN test_user.okko.FILL_customers(100); test_user.okko.FILL_products(100); test_user.okko.FILL_fct_events; END;',
+  START_DATE => SYSTIMESTAMP,
+  REPEAT_INTERVAL => 'FREQ=MINUTELY; INTERVAL=5',
+  END_DATE => SYSTIMESTAMP + INTERVAL '30' day,
+  COMMENTS => 'Insert new data into source DB',
+  ENABLED => TRUE);
 END;
