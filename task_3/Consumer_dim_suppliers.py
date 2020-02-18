6ab8ce0162ca03847602a97123eb45dc0f25e15c
moiseenko1 <savel1997@gmail.com>
2020-02-18 14:39:12 +0300

    Consumers are modified.

--- task_3/Consumer_dim_suppliers.py
+++ task_3/Consumer_dim_suppliers.py
@@ -14,7 +14,7 @@ HOST_IP = "192.168.88.95"
 PORT = "1521"
 SID = "orcl"
 
-TARGET_DB_TABLE_NAME = "DIM_SUPPLIERS " 
+TARGET_DB_TABLE_NAME = "DIM_SUPPLIERS"
 TARGET_DB_USER_NAME = "test_user"
 TARGET_DB_USER_PASSWORD = "test_user"
 
@@ -22,30 +22,33 @@ TARGET_DB_USER_PASSWORD = "test_user"
 def parse(line):
     data = json.loads(line)
     return data['suppliers_id'], data['category'], data['name'], data['country'], data['city'], data['last_update_date']
-    
+
+
 def deserializer():
     return bytes.decode
+
 
 def save_data(rdd):
     if not rdd.isEmpty():
         rdd = rdd.map(lambda m: parse(m[1]))
         df = sqlContext.createDataFrame(rdd)
         df.createOrReplaceTempView("t")
-        result = spark.sql("select _1 as suppliers_id,_2 as category,_3 as name,_4 as country,_5 as city,to_timestamp(_6) as last_update_date from t")
-        
+        result = spark.sql(
+            "select _1 as suppliers_id,_2 as category,_3 as name,_4 as country,_5 as city,to_timestamp(_6) as last_update_date from t")
+
         # Writing to HDFS
         result.write \
             .format("csv") \
             .mode("append") \
             .option("header", "true") \
             .save(HDFS_OUTPUT_PATH)
-        
+
         # Writing to Oracle DB
         result.write \
             .format("jdbc") \
             .mode("append") \
             .option("driver", 'oracle.jdbc.OracleDriver') \
-            .option("url","jdbc:oracle:thin:@{0}:{1}:{2}".format(HOST_IP, PORT, SID)) \
+            .option("url", "jdbc:oracle:thin:@{0}:{1}:{2}".format(HOST_IP, PORT, SID)) \
             .option("dbtable", TARGET_DB_TABLE_NAME) \
             .option("user", TARGET_DB_USER_NAME) \
             .option("password", TARGET_DB_USER_PASSWORD) \
@@ -53,21 +56,24 @@ def save_data(rdd):
     else:
         ssc.stop()
     return rdd
-    
+
+
 def storeOffsetRanges(rdd):
     global offsetRanges
     offsetRanges = rdd.offsetRanges()
     return rdd
-  
+
+
 def printOffsetRanges(rdd):
     for o in offsetRanges:
         f = open('offset_value_dim_suppliers.txt', 'w')
         f.write(str(o.untilOffset))
         f.close()
 
+
 if __name__ == "__main__":
     ssc = StreamingContext(sc, 5)
-    topicPartion = TopicAndPartition(TOPIC,PARTITION)
+    topicPartion = TopicAndPartition(TOPIC, PARTITION)
     f = open('offset_value_dim_suppliers.txt', 'r')
     start = int(f.read())
     f.close()
@@ -75,12 +81,13 @@ if __name__ == "__main__":
     kafkaParams = {"metadata.broker.list": BROKER_LIST}
     kafkaParams["enable.auto.commit"] = "false"
 
-    directKafkaStream = KafkaUtils.createDirectStream(ssc, [TOPIC], kafkaParams, fromOffsets=fromOffset, keyDecoder=deserializer(), valueDecoder=deserializer())
-                                                      
+    directKafkaStream = KafkaUtils.createDirectStream(ssc, [TOPIC], kafkaParams, fromOffsets=fromOffset,
+                                                      keyDecoder=deserializer(), valueDecoder=deserializer())
+
     time.sleep(5)
     directKafkaStream.foreachRDD(lambda x: save_data(x))
     directKafkaStream.transform(storeOffsetRanges) \
-     .foreachRDD(printOffsetRanges)
+        .foreachRDD(printOffsetRanges)
 
     ssc.start()
     ssc.awaitTermination()
