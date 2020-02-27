a493e03aa074c47d88b6ce3742e991de1480db39
AzAcc <zzaaxvat@gmail.com>
2020-02-27 16:37:01 +0300

    Update old_data_delete.sql
    last_patch

--- task_7/old_data_delete.sql
+++ task_7/old_data_delete.sql
@@ -1,3 +1,20 @@
+
+DROP TABLE OLD_DATA_PROD PURGE;
+--Таблица по удалёным данным--
+create table OLD_DATA_PROD(f_date DATE, l_date DATE, proceeds NUMBER);
+
+--Процедура вставки в таблицу OLD_DATA_PROD --
+CREATE OR REPLACE PROCEDURE ODP_INSERT (  
+f_dat OLD_DATA_PROD.f_date%TYPE,  
+l_dat OLD_DATA_PROD.l_date%TYPE,  
+proc OLD_DATA_PROD.proceeds%TYPE)    
+IS  
+BEGIN  
+    INSERT INTO OLD_DATA_PROD VALUES (f_dat, l_dat, proc);  
+    COMMIT;  
+END;  
+/  
+--Процедура очистки таблицы FCT_PROD если число строк больше 1000000--
 create or replace PROCEDURE cleaner
 AS 
     row_count number:=0;
@@ -18,30 +35,31 @@ BEGIN
        select EVENT_TIME into f_date from FCT_PROD where ROWNUM = 1;
        select max(EVENT_TIME) into l_date from FCT_PROD;
        avg_date := f_date + (l_date - f_date)/2;
-       select sum(sold) into procceds from SumPerDay
-       where day between f_date and avg_date;
        DELETE FCT_PROD where EVENT_TIME < avg_date; 
+       select sum(SOLD) into procceds from SumPerDay where DAY BETWEEN to_char(f_date,'dd.mm.yyyy') and to_char(avg_date,'dd.mm.yyyy');
+       ODP_INSERT(f_date, avg_date, procceds);
        SYS.dbms_space.unused_space('TEST_USER','FCT_PROD','TABLE',VAR1,VAR2,VAR3,VAR4,VAR5,VAR6,VAR7);
-       EXECUTE IMMEDIATE 'insert into OLD_DATA_PROD values(d1, avg_date ,procceds)';
-       EXECUTE IMMEDIATE 'ALTER TABLE MILLER.CUSTOMERS DEALLOCATE UNUSED KEEP VAR3';
---     EXECUTE IMMEDIATE 'alter table FCT_PROD deallocate unused';
---	   EXECUTE IMMEDIATE 'alter index FCT_PROD deallocate unused';
+       EXECUTE IMMEDIATE 'ALTER TABLE FCT_PROD DEALLOCATE UNUSED KEEP'|| VAR3;
    end if; 
   COMMIT;
    EXCEPTION WHEN others THEN
    NULL;
 END; 
-/
+--Для теста--
+BEGIN
+    cleaner;
+END;
 
+--Расписание для cleaner_job--
 BEGIN
 DBMS_SCHEDULER.CREATE_SCHEDULE
 ( schedule_name   => 'clean_shedule'
 , start_date      => SYSTIMESTAMP
 , repeat_interval => 'FREQ=WEEKLY; BYDAY=MON,THU; BYHOUR=18; BYMINUTE = 00'
-) ;
+);
 END;
 /
-
+--Программа выполнения cleaner_job--
 BEGIN
 DBMS_SCHEDULER.CREATE_PROGRAM
 ( program_name  => 'clean_program'
@@ -51,7 +69,7 @@ DBMS_SCHEDULER.CREATE_PROGRAM
 );
 END;
 /
-
+--cleaner_job--
 BEGIN
 DBMS_SCHEDULER.CREATE_JOB
 ( job_name      => 'cleaner_job'
@@ -61,4 +79,3 @@ DBMS_SCHEDULER.CREATE_JOB
 );
 END;
 /
-create table OLD_DATA_PROD(f_date DATE, l_date DATE, proceeds int)
