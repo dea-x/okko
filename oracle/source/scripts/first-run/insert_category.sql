d4b362b23b641badbb251cffdcd7ee21116b1386
Pavel Gubkin <57576761+dea-x@users.noreply.github.com>
2020-03-10 15:00:39 +0300

    Create insert_category.sql

---
+++ oracle/source/scripts/first-run/insert_category.sql
@@ -0,0 +1,8 @@
+-- Наполняе справочник категорий --
+
+insert into category values (1, 'Дом');
+insert into category values (2, 'Кухня');
+insert into category values (3, 'Красота');
+insert into category values (4, 'Mobile');
+
+commit;
