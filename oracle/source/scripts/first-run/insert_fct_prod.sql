4a4f0dab099d27767380a50ac3799c5b756fbc6d
Pavel Gubkin <57576761+dea-x@users.noreply.github.com>
2020-03-10 15:11:21 +0300

    Changes in the structure of the project

--- oracle/source/scripts/first-run/insert_fct_prod.sql
+++ oracle/source/scripts/first-run/insert_fct_prod.sql
@@ -27,7 +27,7 @@ begin
         for i in 1..rdn
         loop             
             insert into fct_prod (id, event_id, event_time, product_id, customer_id) values
-                (fct_s_prod.NEXTVAL,  --id события 
+                (fct_s.NEXTVAL,  --id события 
                 decode(trunc(dbms_random.value(1,10)), 1, 1, 2, 1, 3, 1, 4, 1,
                                                        5, 2, 6, 2, 7, 2,
                                                        8, 3,
